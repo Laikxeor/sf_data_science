bd6365a8a7b7976f0dcd9a04e8bf7cc35ea68cf5
Laikxeor <email@example.ru>
2023-01-23 18:54:53 +0300

    Финальное задание

--- game_v2.py
+++ game_v2.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+    """Угадываем число с умом
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
